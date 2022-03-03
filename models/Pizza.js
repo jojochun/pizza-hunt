74b0cc94f86e4982f4a6f42402226526243ff0ac
jochun <jkchun@protonmail.com>
2022-03-02 22:16:13 -1000

    update feature/add-replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -38,7 +38,8 @@ const PizzaSchema = new Schema({
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function () {
-    return this.comments.length;
+    // reduce() takes 2 parameters (accumulator and currentValue).  Returns revised total for next iteration thru the array
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
